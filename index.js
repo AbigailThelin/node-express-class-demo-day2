ae64f98e739730a17ba54cce7970f7dfbe49f4c8
Abigail Thelin <abbyjthelin@gmail.com>
2017-08-16 11:04:43 -0600

    halfway done

--- index.js
+++ index.js
@@ -2,12 +2,22 @@ const express = require('express'),
       bodyParser = require('body-parser'),
       app=express(),
       port=3000;
+      data= require('./data.js')
 
 
 app.use(bodyParser.json())
 
 app.get('/api/student', (req, res)=>{
-
+    if(req.query.grade){
+        data.fiter(e =>{
+            if(e.grade === +req.query.grade){
+                return e
+            }
+        })
+    } else {
+        finalData = data
+    }
+    res.status(200).send(data)
 })
 
 
