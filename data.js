96709956be28b95da4aa0682f85d3face3e1aced
Abigail Thelin <abbyjthelin@gmail.com>
2017-08-16 13:24:22 -0600

    finished morning project

--- data.js
+++ data.js
@@ -1,18 +1,37 @@
 module.exports = [
-    {
-        name: 'sally',
-        grade: 12
-    },
-    {
-        name: 'joe',
-        grade: 2
-    },
-    {
-        name: 'joey',
-        grade: 10
-    },
-    {
-        name: 'jane',
-        grade: 12
-    }
+  {
+    name: 'Sally laRue',
+    grade: 12,
+    age: 18
+  },
+  {
+    name: 'Henry Von Eckleberry',
+    grade: 11,
+    age: 17
+  },
+  {
+    name: 'Jane Bane',
+    grade: 11,
+    age: 17
+  },
+  {
+    name: 'Jane Bane',
+    grade: 9,
+    age: 14
+  },
+  {
+    name: 'Johnny Appleseed',
+    grade: 9,
+    age: 13
+  },
+  {
+    name: 'John Smith',
+    grade: 10,
+    age: 15
+  },
+  {
+    name: 'James Silversteen',
+    grade: 2,
+    age: 16
+  }
 ]
--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express'),
       bodyParser = require('body-parser'),
       app=express(),
-      port=3000;
-      data= require('./data.js')
+      port=3000,
+      data= require('./data.js'),
+      cors = require('cors')
 
 
+app.use(cors())
 app.use(bodyParser.json())
+
+
 
 app.get('/api/student', (req, res)=>{
     if(req.query.grade){
@@ -20,7 +24,102 @@ app.get('/api/student', (req, res)=>{
     res.status(200).send(data)
 })
 
+app.get('/api/getName', (req, res)=>{
+        let students = data.filter(e=>{
+            if(e.name === req.query.name){
+                return e
+            }
+        })
+    res.status(200).send(students)
+    }
+)
 
 
+
+app.post('/api/newStudent', (req, res)=>{
+    //res.end() >> not sending back data but ending request
+    //res.send() >> is sending back data and also ending request
+    data.push(req.body)
+    res.status(200).end()
+})
+
+app.post('/api/admin', (req, res)=>{
+    let admin={
+        username: 'Ironman',
+        password: 'Avengers'
+    }
+    let {username, password } = req.body
+    if(username === admin.username && password === admin.password){
+        res.status(200).redirect('http://google.com')
+    }else{
+        res.status(402).redirect("nope")
+    }
+
+})
+
+
+app.get('/api/age', (req, res)=>{
+    let finalData = data;
+    if(req.query.over){
+        finalData = finalData.filter(e=>{
+            if(e.age > +req.query.over){
+                return e
+            }
+        })
+    }
+    res.status(200).send(finalData)
+})
+
+
+app.get('/api/grade/:grade', (req, res)=>{
+        let finalData= data.filter(e=>{
+            if(e.grade === +req.params.grade){
+                return e
+            }
+        })
+        if(req.query.age){
+            finalData = finalData.filter(e=>{
+                return e.age === +req.query.age
+            })
+        }
+    res.status(200).send(finalData)
+})
+
+
+// app.get('/api/admin/:username/:password', (req, res) => {
+//   // using params so you test it out easier in the browser, it would be better to use req.body with a post or something
+//   let admin = {
+//     username: 'Ironman',
+//     password: 'Av3ngers!'
+//   } 
+//   let { username, password } = req.params
+//   if(username === admin.username && password === admin.password) {
+//     res.status(200).redirect('http://google.com')
+//   } else {
+//     res.status(403).redirect('http://yahoo.com')
+//   }
+// })
+
+
+//=========TWO PARAMETERS IN ONE===============//
+
+// app.get('/api/student', (req, res) => {
+//   let finalData = data;
+//   if(req.query.grade) {
+//     finalData = finalData.filter(e => {
+//       if(e.grade === +req.query.grade[0] || e.grade === +req.query.grade[1]) {
+//         return e
+//       }
+//     })
+//   }
+//   if(req.query.name) {
+//     finalData = finalData.filter(e => {
+//     if(e.name.toLowerCase() === req.query.name.toLowerCase()) {
+//       return e
+//     }
+//   })
+//   }
+//   res.status(200).send(finalData)
+// })
 
 app.listen(port, ()=> console.log(`listening on port ${port}`))
